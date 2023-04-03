47ddcd31f20031158a7d4e9cbad059b15551bbc4
alejandro <alejandrober96@gmail.com>
2023-04-03 16:31:45 -0500

    Add content to booksSlice file

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,5 +1,26 @@
-const booksSlice = () => {
+import { createSlice } from '@reduxjs/toolkit';
 
-}
+const initialState = {
+  books: [],
+};
 
-export default booksSlice;
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      state.books = state.books.push({
+        name: payload.name,
+        author: payload.author,
+        id: payload.id,
+      });
+    },
+    removeBook: (state, { payload }) => {
+      state.books = state.books.filter((book) => book.id !== payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,6 +1,5 @@
-
 const categoriesSlice = () => {
 
-}
+};
 
 export default categoriesSlice;
