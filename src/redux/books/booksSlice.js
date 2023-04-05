5f44c257916eff573716a82c399001167b10bcda
alejandro <alejandrober96@gmail.com>
2023-04-05 15:07:51 -0500

    Add thunk and extra reducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const app = '26VQCgZeIQMuYz9e44m4';
+const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${app}/books`;
+
+export const fetchData = createAsyncThunk(
+  'books/fetchData',
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await axios(url);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
 
 const initialState = {
   books: [],
+  isLoading: false,
+  error: '',
 };
 
 const booksSlice = createSlice({
@@ -18,6 +36,22 @@ const booksSlice = createSlice({
     removeBook: (state, { payload }) => {
       state.books.filter((book) => book.id !== payload.id);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.pending, (state) => ({
+      ...state,
+      isLoading: true,
+    }));
+    builder.addCase(fetchData.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      books: action.payload,
+    }));
+    builder.addCase(fetchData.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
   },
 });
 
