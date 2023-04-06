a06765c41ec2a415aad9b16b90cfc005c2f23559
alejandro <alejandrober96@gmail.com>
2023-04-06 10:31:55 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 const Navbar = () => {
   const links = [
@@ -7,17 +8,23 @@ const Navbar = () => {
   ];
 
   return (
-    <nav>
-      <h1>Bookstore</h1>
-      <ul>
+    <nav className="navbar">
+      <h1 className="navbar-title">Bookstore</h1>
+      <ul className="nav-links">
         {links.map((item) => (
-          <li key={item.text}>
+          <li key={item.text} className="nav-link">
             <NavLink to={item.path}>
               {item.text}
             </NavLink>
           </li>
         ))}
       </ul>
+      <button
+        type="button"
+        className="nav-btn"
+      >
+        <BsFillPersonFill className="person" />
+      </button>
     </nav>
   );
 };
