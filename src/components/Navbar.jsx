3b34c1ae99158bf3b1d0a491ff1f22729985042e
alejandro <alejandrober96@gmail.com>
2023-04-06 12:06:34 -0500

    Final touches and linters

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import { BsFillPersonFill } from 'react-icons/bs';
 
 const Navbar = () => {
   const links = [
-    { path: '/', text: 'Books' },
-    { path: 'Categories', text: 'Categories' },
+    { path: '/', text: 'BOOKS' },
+    { path: 'Categories', text: 'CATEGORIES' },
   ];
 
   return (
     <nav className="navbar">
-      <h1 className="navbar-title">Bookstore</h1>
+      <h1 className="navbar-title">Bookstore CMS</h1>
       <ul className="nav-links">
         {links.map((item) => (
           <li key={item.text} className="nav-link">
-            <NavLink to={item.path}>
+            <NavLink className="nav-link" to={item.path}>
               {item.text}
             </NavLink>
           </li>
